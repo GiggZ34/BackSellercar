6a593c74485cadee00a7b428a73cb30ac76f152d
Matthias <matthias.begot@gmail.com>
2024-09-25 10:59:08 +0200

    add option model

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app.constantes import SellerRoles
 
 
 class Concession(models.Model):
-    concession_zip = models.IntegerField(default=0)
+    zip = models.IntegerField(default=0)
 
 
 class RelationSell(models.Model):
@@ -14,20 +14,28 @@ class RelationSell(models.Model):
         "CarModel", on_delete=models.CASCADE, related_name="relations_sells")
     customer = models.ForeignKey(
         "Customer", on_delete=models.CASCADE, related_name="relations_sells")
-    quantity = models.IntegerField(default=1)
-
+    options = models.ManyToManyField(
+        "Option", related_name="relations_sells"
+    )
 
 class Seller(models.Model):
-    Seller_roles = models.CharField(choices=SellerRoles.choices, max_length=30)
+    roles = models.CharField(choices=SellerRoles.choices, max_length=30)
     concession = models.ForeignKey("Concession", on_delete=models.CASCADE)
 
 
 class CarModel(models.Model):
-    Car_model = models.CharField(max_length=200)
-    Car_price = models.IntegerField(default=0)
-    Car_sell = models.BooleanField(default=False)
+    model = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    sell = models.BooleanField(default=False)
     concession = models.ForeignKey("Concession", on_delete=models.CASCADE, related_name="Cars_models")
 
 
 class Customer(models.Model):
     Customer_name = models.CharField(max_length=200)
+
+
+class Option(models.Model):
+    model = models.ForeignKey("CarModel", on_delete=models.CASCADE, related_name="Options")
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+
