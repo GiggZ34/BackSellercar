d2b4efda960190eef6b21c52bdad1963a91ed3f4
guillaume.rivalan <guillaume.rivalan@teacorbas.gcatrans.com>
2024-09-25 15:09:56 +0200

    add field in customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,4 +2,6 @@ from django.db import models
 
 
 class Customer(models.Model):
-    Customer_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    birthday = models.DateField()
