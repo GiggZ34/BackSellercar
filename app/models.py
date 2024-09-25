016e8ac38706306c2962231de7453fd2e1c9fd96
Matthias <matthias.begot@gmail.com>
2024-09-25 10:53:02 +0200

    init project

--- app/models.py
+++ app/models.py
@@ -9,25 +9,20 @@ class Concession(models.Model):
 
 class RelationSell(models.Model):
     seller = models.ForeignKey(
-        "Seller", on_delete=models.CASCADE, related_name="relations_sells", db_constraint=True)
+        "Seller", on_delete=models.CASCADE, related_name="relations_sells")
     carmodel = models.ForeignKey(
-        "CarModel", on_delete=models.CASCADE, related_name="relations_sells", db_constraint=True)
+        "CarModel", on_delete=models.CASCADE, related_name="relations_sells")
     customer = models.ForeignKey(
-        "Customer", on_delete=models.CASCADE, related_name="relations_sells", db_constraint=True)
+        "Customer", on_delete=models.CASCADE, related_name="relations_sells")
     quantity = models.IntegerField(default=1)
 
 
 class Seller(models.Model):
     Seller_roles = models.CharField(choices=SellerRoles.choices, max_length=30)
     concession = models.ForeignKey("Concession", on_delete=models.CASCADE)
-    cars_sell = models.ManyToManyField(
-        "CarModel",
-        through="RelationSell"
-    )
 
 
 class CarModel(models.Model):
-    Car_brand = models.CharField(max_length=200)
     Car_model = models.CharField(max_length=200)
     Car_price = models.IntegerField(default=0)
     Car_sell = models.BooleanField(default=False)
--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getAllCars', views.getAllCars, name='getAllCars'),
-    path('insertCar', views.insertCar, name='insertCar'),
 ]
