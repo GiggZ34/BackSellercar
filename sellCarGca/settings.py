ff229dff5a17f7235e23562243fc33f7a2767595
emile <emilepicard323@gmail.com>
2024-10-08 15:23:23 +0200

    add silk

--- sellCarGca/settings.py
+++ sellCarGca/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "django_filters",
+    "silk",
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "silk.middleware.SilkyMiddleware",
 ]
 
 ROOT_URLCONF = "sellCarGca.urls"
--- sellCarGca/urls.py
+++ sellCarGca/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path('silk/', include('silk.urls', namespace='silk'))
 ]
